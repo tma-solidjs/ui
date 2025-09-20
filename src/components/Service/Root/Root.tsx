86f76ba18d51459b25c21f04ebdb14de7605aef0
Dmitry Maximyuk <d_maksimyk@vk.com>
2025-09-20 15:01:48 +0500

    feat(v0.0.15): update Root for use global theme and platform variables

--- src/components/Service/Root/Root.tsx
+++ src/components/Service/Root/Root.tsx
@@ -1,12 +1,22 @@
 import "./styles/root.sass";
 
-import { type JSX, type Component } from "solid-js";
+import { type JSX, type Component, onMount } from "solid-js";
+
+import { usePlatform } from "@/hooks";
 
 export interface ImageProps {
   children: JSX.Element;
+  theme: "light" | "dark";
 }
 
 const Root: Component<ImageProps> = (props) => {
+  const platform = usePlatform();
+
+  onMount(() => {
+    document.documentElement.dataset.theme = props.theme;
+    document.documentElement.dataset.platform = platform();
+  });
+
   return props.children;
 };
 
